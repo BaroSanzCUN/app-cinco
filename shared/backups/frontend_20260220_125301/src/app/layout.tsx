2b89d39eecdd7c33c58bf7975aebefb09f6c4746
ADMCINCO <cinco.programacion@gmail.com>
2026-02-20 13:06:03 -0500

    fix: sync package-lock

---
+++ shared/backups/frontend_20260220_125301/src/app/layout.tsx
@@ -0,0 +1,28 @@
+// src/app/layout.tsx
+import './globals.css';
+import "flatpickr/dist/flatpickr.css";
+import { Outfit } from 'next/font/google';
+import { ThemeProvider } from '@/context/ThemeContext';
+import { SidebarProvider } from '@/context/SidebarContext';
+import { Toaster } from '@/components/ui/sonner';
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={`${outfit.className} dark:bg-gray-900`}>
+        <Toaster />
+        <ThemeProvider>
+          <SidebarProvider>{children}</SidebarProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
